29966b5dc2048d4dee576b6e368758a35c24d384
VKI <lib.aca55a@gmail.com>
2014-12-15 20:59:28 +0200

    old scala fixes

--- src/test/scala/model/ImmutableObject.scala
+++ src/test/scala/model/ImmutableObject.scala
@@ -18,7 +18,7 @@ object Pipelining {
 import Pipelining._
 
 case class Employee(id: Int = -1, val name: String, val salary: Double) {
-  def update_salary(update: Double => Double) =
+  def UpdateSalary(update: Double => Double) =
     this.copy(salary = update(salary))
 }
 
@@ -28,28 +28,29 @@ case class Company (
   autoid: Int = 1
 ) {
   self  =>
-  
-
-  def add_employee(employee: Employee) = {
+ 
+  def AddEmployee(employee: Employee) = {
     store_employee(employee.copy(id = self.autoid)).
       update_autoid { id => id + 1 }
   }
 
-  def store_employee(employee: Employee) = {
+  def GetEmployee(id: Int) = employees.get(id)
+
+  def UpdateEmployee(id: Int, update: Employee => Employee) = {
+    GetEmployee(id) |> maybe_update_employee(update)
+  }
+
+  
+  private def store_employee(employee: Employee) = {
     update_employees( dict => dict + ((employee.id, employee)) )
   }
 
-  def update_employees(update: immutable.Map[Int, Employee] => immutable.Map[Int, Employee]) =
+  private def update_employees(update: immutable.Map[Int, Employee] => immutable.Map[Int, Employee]) =
     this.copy(employees = update(employees))
 
-  def update_autoid(update: Int => Int) = this.copy(autoid = update(autoid))
-  def get_employee(id: Int) = employees.get(id)
+  private def update_autoid(update: Int => Int) = this.copy(autoid = update(autoid))
 
-  def update_employee(id: Int, update: Employee => Employee) = {
-    get_employee(id) |> maybe_update_employee(update)
-  }
-
-  def maybe_update_employee(update: Employee => Employee)(employee: Option[Employee]) =
+  private def maybe_update_employee(update: Employee => Employee)(employee: Option[Employee]) =
     employee match {
       case None => None
       case Some(e) =>
@@ -58,9 +59,9 @@ case class Company (
 }
 
 object Company {
-  def factory(name: String, input: List[(String, Double)]) =
+  def Factory(name: String, input: List[(String, Double)]) =
     input.foldLeft (Company(name)) { case (company, data) =>
-      company.add_employee(Employee(name = data._1, salary = data._2))
+      company.AddEmployee(Employee(name = data._1, salary = data._2))
     }
     
 }
@@ -70,13 +71,13 @@ class ImmutableObjectSpec extends WordSpecLike with Matchers {
   "Immutable Objct" should {
     "CRUD imutably" in {
       val c = Company(name = "Intech").
-        add_employee(Employee(name = "Peter Gibbons", salary = 10000)).
-        add_employee(Employee(name = "Michael Bolton", salary = 20000))
+        AddEmployee(Employee(name = "Peter Gibbons", salary = 10000)).
+        AddEmployee(Employee(name = "Michael Bolton", salary = 20000))
 
-      val e1 = c.get_employee(1).get
+      val e1 = c.GetEmployee(1).get
 
-      val c1 = c.update_employee(1, { case employee =>
-        employee.update_salary { s => s * 1.2 }})
+      val c1 = c.UpdateEmployee(1, { case employee =>
+        employee.UpdateSalary { s => s * 1.2 }})
 
       e1.salary should equal (10000)
     }
@@ -87,8 +88,8 @@ class ImmutableObjectSpec extends WordSpecLike with Matchers {
         ("Michael Bolton", 20000.0)
       )
 
-      val c = Company.factory("Intech", input)
-      c.get_employee(1).get.name should equal("Peter Gibbons")
+      val c = Company.Factory("Intech", input)
+      c.GetEmployee(1).get.name should equal("Peter Gibbons")
     }
   }
 }
